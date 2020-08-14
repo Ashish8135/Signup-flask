61df79326d8cf0d5f510c536e63578a613c179a9
Ashish anand <50195305+Ashish8135@users.noreply.github.com>
2020-08-14 12:40:13 +0530

    created login form

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
-from flask import Flask
-
+from flask import Flask,render_template,request,redirect,url_for
+from flaskext.mysql import MySQL
+import pymysql
+mysql = MySQL()
 app=Flask(__name__)
+mysql.init_app(app)
+
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='root'
+app.config['MYSQL_DATABASE_DB']='login_db '
+app.config['MYSQL_DATABASE_HOST']='localhost'
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/',methods=['POST','GET'])
+def index():
+    return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login',methods=['POST','GET']) 
 def login():
-    return render_template('login.html')
+    conn=mysql.connect()
+    cursor=conn.cursor(pymysql.cursor.Dictcursor)
+    #outpu msg if something wrong
+    msg=''
+    #chekc if password and username exists in submitted form
+    if request.method=='POST' and 'username' in request.form and 'password ' in request.form:
+        username=request.form['username']
+        password=request.form['password']
+        #check if acount exist in mysql or in database
+        cursor.execute('select * from  users where username=%s and password=%s',(username,password))
+        user=cursor.fetchone()
+        return redirect(url_for('profile'))
 
-@app.route('/register')
+        if user:
+            session['id']=user['id']
+            session['username']=user['username']
+            return redirect(url_for('profile'))
+        else:
+            msg="Incorrect username/password" 
+    return render_template('login.html',msg=msg)
+
+@app.route('/register',methods=['POST','GET'])
 def register():
     return render_template('register.html')
 
-@app.route('/profile')
+@app.route('/profile',methods=['POST','GET'])
 def profile():
     return render_template('profile.html')
 
